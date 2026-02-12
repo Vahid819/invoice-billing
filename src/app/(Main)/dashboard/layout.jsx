2e3eebf1c4e2a7a658fb4195d24e68a7b3b1fa4f
vahid819 <vahidmomin1819@gmail.com>
2026-02-12 19:36:22 +0530

    authentication updated

--- src/app/(Main)/dashboard/layout.jsx
+++ src/app/(Main)/dashboard/layout.jsx
@@ -1,27 +1,21 @@
-import { getServerSession } from "next-auth"
-import { redirect } from "next/navigation"
-import authOptions from "@/lib/auth"
-
 import { SidebarProvider } from "@/components/ui/sidebar"
 import AppSidebar from "@/components/app-sidebar"
 import { ThemeProvider } from "@/components/theme-provider"
+import AuthGuard from "@/components/auth-guard"
 
-export default async function DashboardLayout({ children }) {
-  const session = await getServerSession(authOptions)
-
-  if (!session) redirect("/login")
-
+export default function DashboardLayout({ children }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <SidebarProvider>
-        <div className="flex min-h-screen w-full overflow-hidden">
-          <AppSidebar />
-
-          <main className="flex-1 overflow-y-auto overflow-x-hidden">
-            <div className="p-6">{children}</div>
-          </main>
-        </div>
-      </SidebarProvider>
+      <AuthGuard>
+        <SidebarProvider>
+          <div className="flex min-h-screen w-full overflow-hidden">
+            <AppSidebar />
+            <main className="flex-1 overflow-x-hidden overflow-y-auto p-6">
+              {children}
+            </main>
+          </div>
+        </SidebarProvider>
+      </AuthGuard>
     </ThemeProvider>
   )
 }
--- src/proxy.js
+++ src/proxy.js
@@ -1,19 +1,10 @@
 import { withAuth } from "next-auth/middleware"
 
-export default withAuth(
-  function middleware(req) {
-    // You can add custom logic here later if needed
-      // return NextResponse.next()
-
-
+export default withAuth({
+  pages: {
+    signIn: "/login",
   },
-  {
-    pages: {
-      signIn: "/login",
-      error: "/login?error=1",
-    },
-  }
-)
+})
 
 export const config = {
   matcher: ["/dashboard/:path*"],
